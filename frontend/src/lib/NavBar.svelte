f480bab7d819d825209d1a5b2687036c153ca359
Fury_101 <73406313+Fury-101@users.noreply.github.com>
2022-10-02 14:25:59 -0700

    more stuff for home page and assets

--- frontend/src/lib/NavBar.svelte
+++ frontend/src/lib/NavBar.svelte
@@ -3,18 +3,17 @@
 
     onMount(() => {
         const navEl = document.getElementsByTagName("nav")[0];
-        window.onscroll = () => {
+        document.addEventListener('scroll', () => {
             if ((document.body.scrollTop || document.documentElement.scrollTop) > 0)
                 navEl.style.transform = "translateY(-100%)";
             else
                 navEl.style.transform = "translateY(0%)";
-        }
+        });
     });
 </script>
 
 <nav>
     <img src="/src/assets/logo_banner.png" alt="logo_banner">
-    <div class="flex-spacer"></div>
     <a href="/about">About</a>
     <a href="/resources">Resources</a>
     <a href="/partners">Partnerships</a>
@@ -24,27 +23,42 @@
 
 <style>
     nav {
+        z-index: 1;
         position: absolute;
         width: 100%;
         background: white;
         border-bottom: 2px solid #dee2e6; 
         display: flex;
         flex-direction: row;
+        place-items: center;
         transition: transform 0.5s;
+        animation: slidein 0.5s;
+    }
+    @keyframes slidein {
+        from {
+            transform: translateY(-100%);
+        }
+        to {
+            transform: translateY(0%);
+        }
     }
     nav > img {
-        max-height: 50px;
+        max-height: 64px;
         padding-top: 0.5rem;
         padding-bottom: 0.5rem;
         padding-left: 1rem;
         padding-right: 1rem;
     }
     nav > a {
+        color: black;
         position: relative;
         padding-top: 0.75rem;
         padding-bottom: 0.75rem;
         padding-left: 1rem;
         padding-right: 1rem;
+    }
+    nav > a:first-of-type {
+        margin-left: auto;
     }
     nav > a::after {
         content: '';
@@ -55,13 +69,10 @@
         width: 0%;
         transition: width 0.4s;
         height: 2px;
-        background: red;
+        background: var(--primary);
         z-index: 1;
     }
     nav > a:hover::after {
         width: calc(100% - 2rem);
-    }
-    .flex-spacer {
-        flex-grow: 1;
     }
 </style>
