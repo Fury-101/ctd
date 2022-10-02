50bd31c4abf452e8dc5869a69f54ba0066f2a720
Fury_101 <73406313+Fury-101@users.noreply.github.com>
2022-10-02 11:11:47 -0700

    setup frontend

---
+++ frontend/src/lib/NavBar.svelte
@@ -0,0 +1,63 @@
+<script>
+    import { onMount } from 'svelte';
+
+    onMount(() => {
+        const navEl = document.getElementsByTagName("nav")[0];
+        window.onscroll = () => {
+            if ((document.body.scrollTop || document.documentElement.scrollTop) > 0)
+                navEl.style.transform = "translateY(-100%)";
+            else
+                navEl.style.transform = "translateY(0%)";
+        }
+    });
+</script>
+
+<nav>
+    <img src="/src/assets/logobanner.png" alt="logobanner">
+    <div class="flex-spacer"></div>
+    <a href="/about">About</a>
+    <a href="/resources">Resources</a>
+    <a href="/partners">Partnerships</a>
+    <a href="/join">Join Us</a>
+    <a href="/news">News</a>
+</nav>
+
+<style>
+    nav {
+        position: absolute;
+        width: 100%;
+        background: white;
+        border-bottom: 2px solid #dee2e6; 
+        display: flex;
+        flex-direction: row;
+        transition: transform 0.5s;
+    }
+    nav > img {
+        max-height: 50px;
+    }
+    nav > * {
+        position: relative;
+        padding-top: 0.75rem;
+        padding-bottom: 0.75rem;
+        padding-left: 1rem;
+        padding-right: 1rem;
+    }
+    nav > a::after {
+        content: '';
+        position: absolute;
+        bottom: 0.5rem;
+        left: 50%;
+        transform: translateX(-50%);
+        width: 0%;
+        transition: width 0.4s;
+        height: 2px;
+        background: red;
+        z-index: 1;
+    }
+    nav > a:hover::after {
+        width: calc(100% - 2rem);
+    }
+    .flex-spacer {
+        flex-grow: 1;
+    }
+</style>
---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,29 @@
+<section id="s1">
+    <div>
+        <h1>Closing The Divide</h1>
+        <h2>Dedicated to increasing technological literacy for low-income families.</h2>
+    </div>
+</section>
+<section>
+
+</section>
+
+<style>
+    #s1 {
+        background-image: url("/src/assets/s1_bg.png");
+        color: white;
+    }
+    #s1 > div {
+        padding-top: calc(50vh - calc(2 * 96px));
+        padding-left: 32px;
+    }
+    #s1 > div > h1 {
+        font-size: 96px;
+    }
+    #s1 > div > h2 {
+        font-size: 32px;
+    }
+    section {
+        height: 100vh;
+    }
+</style>
